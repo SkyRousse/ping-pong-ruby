7d8d63985b138fa708807396c7e23c089d53d55b
sr <skyrousse@gmail.com>
2016-06-27 16:39:22 -0700

    add project files and folders

---
+++ lib/ping_pong.rb
@@ -0,0 +1,18 @@
+class Fixnum
+  define_method(:ping_pong?) do
+    numbers = (1..self)
+    ping_pongs = Array.new
+    numbers.each() do |number|
+      if number.%(15).eql?(0)
+        ping_pongs.push("pingpong")
+      elsif number.%(5).eql?(0)
+        ping_pongs.push("pong")
+      elsif number.%(3).eql?(0)
+        ping_pongs.push("ping")
+      else
+        ping_pongs.push(number)
+      end
+    end
+    ping_pongs
+  end
+end
---
+++ spec/ping_pong_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('ping_pong')
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+
+describe('Fixnum#ping_pong') do
+  it("is ping for a number divisible by 3") do
+    expect(3.ping_pong()).to(eq(["ping"]))
+  end
+  it("is pong for a number divisible by 5") do
+    expect(5.ping_pong()).to(eq(["pong"]))
+  end
+  it("is ping for a number divisible by 3") do
+    expect(15.ping_pong()).to(eq(["ping-pong"]))
+  end
+end
